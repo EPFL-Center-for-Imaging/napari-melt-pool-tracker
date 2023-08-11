e8dd8bbed992af7f6d26cfd03501e971443bd799
faymanns <florian.aymanns@t-online.de>
2023-08-11 11:59:29 +0200

    Test widgets by calling one butten after another

--- src/napari_melt_pool_tracker/_tests/test_widget.py
+++ src/napari_melt_pool_tracker/_tests/test_widget.py
@@ -8,14 +8,19 @@ from napari_melt_pool_tracker import MeltPoolTrackerQWidget
 def test_example_q_widget(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
+    viewer.add_image(np.random.random((100, 100, 100)))
 
     # create our widget, passing in the viewer
-    my_widget = MeltPoolTrackerQWidget(viewer)
+    widget = MeltPoolTrackerQWidget(viewer)
 
-    # call our widget method
-    my_widget._on_click()
+    # test widget methods
+    widget._split()
+    widget._determine_laser_speed_and_position()
+    widget._reslice_with_moving_window()
+    widget._filter()
+    widget._calculate_radial_gradient()
+    widget._annotate_surface_features()
 
     # read captured output and check that it's as we expected
     captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
+    assert captured.out == ""
