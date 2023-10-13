3657b6a64d752246e895ef33b22f58d3acf4b06d
faymanns <florian.aymanns@t-online.de>
2023-10-13 09:37:19 +0200

    Fix widget test by seting the input comboboxes for each step
    The test was failing because it relied on the fact that all output layers are named correctly and that the next step uses the correct input layer just based on the name. This does not work anymore since each step now as a drop down menu to select the input layers.

--- src/napari_melt_pool_tracker/_tests/test_widget.py
+++ src/napari_melt_pool_tracker/_tests/test_widget.py
@@ -8,17 +8,35 @@ from napari_melt_pool_tracker import MeltPoolTrackerQWidget
 def test_example_q_widget(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100, 100)))
+    viewer.add_image(np.random.random((100, 100, 100)), name="test_image")
 
     # create our widget, passing in the viewer
     widget = MeltPoolTrackerQWidget(viewer)
 
     # test widget methods
+    widget.split_groupbox.comboboxs["Input"].setCurrentText("test_image")
     widget._split()
+    widget.speed_pos_groupbox.comboboxs["Input"].setCurrentText("test_image_0")
     widget._determine_laser_speed_and_position()
+    widget.window_groupbox.comboboxs["Stack"].setCurrentText("test_image_0")
+    widget.window_groupbox.comboboxs["Line"].setCurrentText(
+        "test_image_0_line"
+    )
     widget._reslice_with_moving_window()
+    widget.filter_groupbox.comboboxs["Input"].setCurrentText(
+        "test_image_0_resliced"
+    )
     widget._filter()
+    widget.radial_groupbox.comboboxs["Input"].setCurrentText(
+        "test_image_0_resliced_filtered"
+    )
     widget._calculate_radial_gradient()
+    widget.annotate_surface_groupbox.comboboxs["Input"].setCurrentText(
+        "test_image_0_resliced_filtered_radial_gradient"
+    )
+    widget.annotate_surface_groupbox.comboboxs["Surface"].setCurrentText(
+        "test_image_0_resliced_filtered"
+    )
     widget._annotate_surface_features()
 
     # read captured output and check that it's as we expected
