18e3a9746c36bde2b8f7c311b1a49c4a115a0583
faymanns <florian.aymanns@t-online.de>
2023-10-13 10:45:38 +0200

    Test sample data generation

--- src/napari_melt_pool_tracker/_tests/test_sample_data.py
+++ src/napari_melt_pool_tracker/_tests/test_sample_data.py
@@ -1,7 +1,18 @@
-# from napari_melt_pool_tracker import make_sample_data
+import numpy as np
 
-# add your tests here...
+from napari_melt_pool_tracker import make_sample_data
 
 
-def test_something():
-    pass
+def test_make_sample_data():
+    returned_value = make_sample_data()
+    assert isinstance(returned_value, list)
+    for layer_data_tuple in returned_value:
+        # Check that it is a layer data tuple
+        assert isinstance(layer_data_tuple, tuple)
+        assert len(layer_data_tuple) == 2
+        assert isinstance(layer_data_tuple[0], np.ndarray)
+        assert isinstance(layer_data_tuple[1], dict)
+
+        # Check that the data is 3D
+        data = layer_data_tuple[0]
+        assert data.ndim == 3
