c470e222ca4579e789c54dfa1281f02e185389ba
faymanns <florian.aymanns@t-online.de>
2023-10-13 10:20:57 +0200

    Remove _writer

--- src/napari_melt_pool_tracker/__init__.py
+++ src/napari_melt_pool_tracker/__init__.py
@@ -1,15 +1,12 @@
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
 from ._widget import MeltPoolTrackerQWidget
-from ._writer import write_multiple, write_single_image
 
 __version__ = "0.0.1"
 
 
 __all__ = (
     "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
     "make_sample_data",
     "MeltPoolTrackerQWidget",
 )
